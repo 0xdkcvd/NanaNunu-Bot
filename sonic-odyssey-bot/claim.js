ce9a4764d5aabc07eaa34c8c87c0e35ffa874db2
DK <31103434+0xdkcvd@users.noreply.github.com>
2024-08-11 01:11:29 +0800

    Update claim.js

--- sonic-odyssey-bot/claim.js
+++ sonic-odyssey-bot/claim.js
@@ -9,6 +9,7 @@ const { HEADERS } = require('./src/headers');
 const { displayHeader } = require('./src/displayUtils');
 const readlineSync = require('readline-sync');
 const moment = require('moment');
+const cron = require('node-cron');
 
 const PRIVATE_KEYS = JSON.parse(fs.readFileSync('privateKeys.json', 'utf-8'));
 
@@ -21,30 +22,19 @@ async function getToken(privateKey) {
   try {
     const { data } = await axios({
       url: 'https://odyssey-api-beta.sonic.game/auth/sonic/challenge',
-      params: {
-        wallet: getKeypair(privateKey).publicKey,
-      },
+      params: { wallet: getKeypair(privateKey).publicKey },
       headers: HEADERS,
     });
 
-    const sign = nacl.sign.detached(
-      Buffer.from(data.data),
-      getKeypair(privateKey).secretKey
-    );
+    const sign = nacl.sign.detached(Buffer.from(data.data), getKeypair(privateKey).secretKey);
     const signature = Buffer.from(sign).toString('base64');
     const publicKey = getKeypair(privateKey).publicKey;
-    const encodedPublicKey = Buffer.from(publicKey.toBytes()).toString(
-      'base64'
-    );
+    const encodedPublicKey = Buffer.from(publicKey.toBytes()).toString('base64');
     const response = await axios({
       url: 'https://odyssey-api-beta.sonic.game/auth/sonic/authorize',
       method: 'POST',
       headers: HEADERS,
-      data: {
-        address: publicKey,
-        address_encoded: encodedPublicKey,
-        signature,
-      },
+      data: { address: publicKey, address_encoded: encodedPublicKey, signature },
     });
 
     return response.data.data.token;
@@ -76,7 +66,7 @@ async function doTransactions(tx, keypair, retries = 3) {
   } catch (error) {
     if (retries > 0) {
       console.log(`Retrying transaction... (${retries} retries left)`.yellow);
-      await new Promise((res) => setTimeout(res, 1000));
+      await new Promise((res) => setTimeout(res, 2000));
       return doTransactions(tx, keypair, retries - 1);
     } else {
       console.log(`Error in transaction: ${error}`.red);
@@ -101,18 +91,14 @@ async function openMysteryBox(token, keypair, retries = 3) {
       url: 'https://odyssey-api-beta.sonic.game/user/rewards/mystery-box/open',
       method: 'POST',
       headers: { ...HEADERS, Authorization: token },
-      data: {
-        hash: signature,
-      },
+      data: { hash: signature },
     });
 
     return response.data;
   } catch (error) {
     if (retries > 0) {
-      console.log(
-        `Retrying opening mystery box... (${retries} retries left)`.yellow
-      );
-      await new Promise((res) => setTimeout(res, 1000));
+      console.log(`Retrying opening mystery box... (${retries} retries left)`.yellow);
+      await new Promise((res) => setTimeout(res, 2000));
       return openMysteryBox(token, keypair, retries - 1);
     } else {
       console.log(`Error opening mystery box: ${error}`.red);
@@ -131,9 +117,7 @@ async function processPrivateKey(privateKey) {
       const balance = profile.wallet_balance / solana.LAMPORTS_PER_SOL;
       const ringBalance = profile.ring;
       const availableBoxes = profile.ring_monitor;
-      console.log(
-        `Hello ${publicKey}! Welcome to our bot. Here are your details:`.green
-      );
+      console.log(`Address ${publicKey}! Details:`.green);
       console.log(`Solana Balance: ${balance} SOL`.green);
       console.log(`Ring Balance: ${ringBalance}`.green);
       console.log(`Available Box(es): ${availableBoxes}`.green);
@@ -142,49 +126,30 @@ async function processPrivateKey(privateKey) {
       // Automatically claim daily rewards
       console.log(`[ ${moment().format('HH:mm:ss')} ] Please wait...`.yellow);
       await dailyClaim(token);
-      console.log(
-        `[ ${moment().format('HH:mm:ss')} ] Daily claim completed!`.cyan
-      );
+      console.log(`[ ${moment().format('HH:mm:ss')} ] Daily claim completed!`.cyan);
 
       // Automatically open mystery boxes
-      console.log(
-        `[ ${moment().format('HH:mm:ss')} ] Please wait to open boxes...`.yellow
-      );
+      console.log(`[ ${moment().format('HH:mm:ss')} ] Please wait to open boxes...`.yellow);
 
       // Opening all available mystery boxes
       for (let i = 0; i < availableBoxes; i++) {
         const openedBox = await openMysteryBox(token, getKeypair(privateKey));
         if (openedBox.data.success) {
-          console.log(
-            `[ ${moment().format('HH:mm:ss')} ] Box opened successfully! Status: ${
-              openedBox.status
-            } | Amount: ${openedBox.data.amount}`.green
-          );
+          console.log(`[ ${moment().format('HH:mm:ss')} ] Box opened successfully! Status: ${openedBox.status} | Amount: ${openedBox.data.amount}`.green);
         }
       }
 
-      console.log(
-        `[ ${moment().format('HH:mm:ss')} ] Box opening completed!`.cyan
-      );
+      console.log(`[ ${moment().format('HH:mm:ss')} ] Box opening completed!`.cyan);
 
       // Automatically perform daily login
       console.log(`[ ${moment().format('HH:mm:ss')} ] Please wait for daily login...`.yellow);
       const claimLogin = await dailyLogin(token, getKeypair(privateKey));
       if (claimLogin) {
-        console.log(
-          `[ ${moment().format('HH:mm:ss')} ] Daily login completed! Status: ${
-            claimLogin.status
-          } | Accumulative Days: ${claimLogin.data.accumulative_days}`.cyan
-        );
+        console.log(`[ ${moment().format('HH:mm:ss')} ] Daily login completed! Status: ${claimLogin.status} | Accumulative Days: ${claimLogin.data.accumulative_days}`.cyan);
       }
-      console.log(
-        `[ ${moment().format('HH:mm:ss')} ] All automated tasks completed for ${publicKey}!`.green
-      );
+      console.log(`[ ${moment().format('HH:mm:ss')} ] All automated tasks completed for ${publicKey}!`.green);
     } else {
-      console.log(
-        `There might be errors if you don't have sufficient balance or the RPC is down. Please ensure your balance is sufficient and your connection is stable`
-          .red
-      );
+      console.log(`There might be errors if you don't have sufficient balance or the RPC is down. Please ensure your balance is sufficient and your connection is stable`.red);
     }
   } catch (error) {
     console.log(`Error processing private key: ${error}`.red);
@@ -202,11 +167,7 @@ async function fetchDaily(token) {
 
     return data.data.total_transactions;
   } catch (error) {
-    console.log(
-      `[ ${moment().format('HH:mm:ss')} ] Error in daily fetching: ${
-        error.response.data.message
-      }`.red
-    );
+    console.log(`[ ${moment().format('HH:mm:ss')} ] Error in daily fetching: ${error.response.data.message}`.red);
   }
 }
 
@@ -217,11 +178,7 @@ async function dailyClaim(token) {
   try {
     const fetchDailyResponse = await fetchDaily(token);
 
-    console.log(
-      `[ ${moment().format(
-        'HH:mm:ss'
-      )} ] Your total transactions: ${fetchDailyResponse}`.blue
-    );
+    console.log(`[ ${moment().format('HH:mm:ss')} ] Your total transactions: ${fetchDailyResponse}`.blue);
 
     if (fetchDailyResponse > 10) {
       while (counter <= maxCounter) {
@@ -230,51 +187,24 @@ async function dailyClaim(token) {
             url: 'https://odyssey-api.sonic.game/user/transactions/rewards/claim',
             method: 'POST',
             headers: { ...HEADERS, Authorization: token },
-            data: {
-              stage: counter,
-            },
+            data: { stage: counter },
           });
 
-          console.log(
-            `[ ${moment().format(
-              'HH:mm:ss'
-            )} ] Daily claim for stage ${counter} has been successful! Stage: ${counter} | Status: ${
-              data.data.claimed
-            }`.green
-          );
+          console.log(`[ ${moment().format('HH:mm:ss')} ] Daily claim for stage ${counter} has been successful! Stage: ${counter} | Status: ${data.data.claimed}`.green);
 
           counter++;
         } catch (error) {
           if (error.response.data.message === 'interact task not finished') {
-            console.log(
-              `[ ${moment().format(
-                'HH:mm:ss'
-              )} ] Error claiming for stage ${counter}: ${
-                error.response.data.message
-              }`.red
-            );
+            console.log(`[ ${moment().format('HH:mm:ss')} ] Error claiming for stage ${counter}: ${error.response.data.message}`.red);
             counter++;
-          } else if (
-            error.response &&
-            (error.response.data.code === 100015 ||
-              error.response.data.code === 100016)
-          ) {
-            console.log(
-              `[ ${moment().format(
-                'HH:mm:ss'
-              )} ] Already claimed for stage ${counter}, proceeding to the next stage...`
-                .cyan
-            );
+          } else if (error.response && (error.response.data.code === 100015 || error.response.data.code === 100016)) {
+            console.log(`[ ${moment().format('HH:mm:ss')} ] Already claimed for stage ${counter}, proceeding to the next stage...`.cyan);
             counter++;
           } else {
-            console.log(
-              `[ ${moment().format('HH:mm:ss')} ] Error claiming: ${
-                error.response.data.message
-              }`.red
-            );
+            console.log(`[ ${moment().format('HH:mm:ss')} ] Error claiming: ${error.response.data.message}`.red);
           }
         } finally {
-          await delay(1000);
+          await delay(2000);
         }
       }
 
@@ -283,11 +213,7 @@ async function dailyClaim(token) {
       throw new Error('Not enough transactions to claim rewards.');
     }
   } catch (error) {
-    console.log(
-      `[ ${moment().format('HH:mm:ss')} ] Error in daily claim: ${
-        error.message
-      }`.red
-    );
+    console.log(`[ ${moment().format('HH:mm:ss')} ] Error in daily claim: ${error.message}`.red);
   }
 }
 
@@ -308,60 +234,51 @@ async function dailyLogin(token, keypair, retries = 3) {
       url: 'https://odyssey-api-beta.sonic.game/user/check-in',
       method: 'POST',
       headers: { ...HEADERS, Authorization: token },
-      data: {
-        hash: signature,
-      },
+      data: { hash: signature },
     });
 
     return response.data;
   } catch (error) {
     if (error.response.data.message === 'current account already checked in') {
-      console.log(
-        `[ ${moment().format('HH:mm:ss')} ] Error in daily login: ${
-          error.response.data.message
-        }`.red
-      );
+      console.log(`[ ${moment().format('HH:mm:ss')} ] Error in daily login: ${error.response.data.message}`.red);
     } else {
-      console.log(
-        `[ ${moment().format('HH:mm:ss')} ] Error claiming: ${
-          error.response.data.message
-        }`.red
-      );
+      console.log(`[ ${moment().format('HH:mm:ss')} ] Error claiming: ${error.response.data.message}`.red);
     }
   }
 }
+
+const getNextRunTime = () => {
+  const now = moment.utc();
+  const nextRun = now.clone().startOf('day').add(10, 'hours');
+  if (now.isAfter(nextRun)) {
+    nextRun.add(1, 'day');
+  }
+  return nextRun;
+};
+
+const displayNextRunTime = () => {
+  const nextRunTime = getNextRunTime();
+  console.log(`Next run: ${nextRunTime.format('DD-MM-YYYY HH:mm:ss')} UTC.`);
+};
 
 async function startBot() {
-  try {
-    displayHeader();
-    console.log(`[ ${moment().format('YYYY-MM-DD HH:mm:ss')} ] Starting bot...`);
-    for (let i = 0; i < PRIVATE_KEYS.length; i++) {
-      const privateKey = PRIVATE_KEYS[i];
-      await processPrivateKey(privateKey);
-      if (i < PRIVATE_KEYS.length - 1) {
-        const continueNext = true; // In production, this would typically be set by user interaction or condition
-        if (!continueNext) break;
-      }
-    }
-    console.log(`[ ${moment().format('YYYY-MM-DD HH:mm:ss')} ] All private keys processed.`.cyan);
-  } catch (error) {
-    console.log(`Error in bot operation: ${error}`.red);
-  } finally {
-    const nextRunTime = moment().add(24, 'hours').format('YYYY-MM-DD HH:mm:ss');
-    console.log(
-      `Next run scheduled at ${nextRunTime}. `.white
-    );
+  for (const privateKey of PRIVATE_KEYS) {
+    await processPrivateKey(privateKey);
   }
+  
+
+  displayNextRunTime();
 }
 
-// Initial start of the bot
+cron.schedule('0 10 * * *', async () => {
+  console.log(`[ ${moment().format('DD-MM-YYYY HH:mm:ss')} ] Running scheduled bot task.`);
+  await startBot();
+}, {
+  scheduled: true,
+  timezone: "UTC"
+});
+
+// Log waktu jadwal pertama
+displayNextRunTime();
+
 startBot();
-
-// Function to set interval for every 24 hours (86400000 milliseconds)
-setInterval(() => {
-  console.log(`[ ${moment().format('YYYY-MM-DD HH:mm:ss')} ] Starting bot again after 24 hours.`)
-  startBot();
-}, 86400000);
-
-// Log initial scheduled run time
-console.log(`Initial run scheduled at ${moment().format('YYYY-MM-DD HH:mm:ss')}.`);
